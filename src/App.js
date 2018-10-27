3bbbb43bbd8964b02ea3819d8d92884ad4e24f69
seen2 <tiwarietc@gmail.com>
2018-10-28 02:59:06 +0530

    donw with front ends

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Particles from 'react-particles-js';
 import './App.css';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
+import Rank from './components/rank/Rank';
 import ImageForm from './components/imageForm/ImageForm';
 
 const particlesOptions = {
@@ -23,13 +24,15 @@ class App extends Component {
   render() {
     return (
 			<div className="App" >
-				<Navigation  />
-				<Logo/>
-				<ImageForm/>
 				<Particles 
 					className='particles'
 					params={particlesOptions}
 				/>
+				
+				<Navigation  />
+				<Logo/>
+				<Rank />
+				<ImageForm/>
 				{/*
 				<FaceRecognition/>
 				*/}
