13848bbcbd78acea6bfcc45ed258355dd687de2a
seen2 <tiwarietc@gmail.com>
2018-10-28 02:41:28 +0530

    done with logo

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Tilt from 'react-tilt';
+import brain from './brain.png';
+
+const Logo=()=> {
+    return (
+			<div style={{padding:'20px'}} >
+				<Tilt className="Tilt" options={{ max : 25 }} style={logoStyle} >
+					<div className="Tilt-inner">
+						<img height={"80px"} width={"120px"} src={brain} alt="logo"/> 
+					</div>
+				</Tilt>
+      </div>
+    );
+}  
+const logoStyle={
+	margin:'10',
+	textAlign:'center',
+	padding:'30px',
+	height:'150px',
+	width:'120px',
+	backgroundImage: 'linear-gradient(to right, grey ,red)',
+	border:'1px solid grey'
+}
+export default Logo;
